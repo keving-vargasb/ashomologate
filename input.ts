6a206a72a444a1c9ff15485bcb8797b865f5ffbb
keving-vargasb <keving.vargasb@gmail.com>
2021-06-17 09:12:51 -0500

    1 file added

---
+++ input.ts
@@ -0,0 +1,17 @@
+const applicationExample = {
+  _id: 'abc123',
+  status: {
+    id: 'started',
+    value: 'Iniciado'
+  },
+  agent: {
+    uid: 'agent1'
+  },
+  user: {
+    uid: 'user1',
+    email: 'user1@gmail.com',
+    phone: '+573123010242'
+  }
+};
+
+export { applicationExample };
